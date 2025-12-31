65d55f33cd79445b5777f654c938f697dfd702c0
parkson14-taiwan <parkson14@gmail.com>
2025-12-31 15:01:07 +0800

    Configure Firebase project settings

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
-  projectId: "YOUR_FIREBASE_PROJECT_ID",
-  storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_FIREBASE_SENDER_ID",
-  appId: "YOUR_FIREBASE_APP_ID",
+  apiKey: "AIzaSyByrugAyxH1aRe385WzPwS6trHHvaYrRvI",
+  authDomain: "part-time-table.firebaseapp.com",
+  projectId: "part-time-table",
+  storageBucket: "part-time-table.firebasestorage.app",
+  messagingSenderId: "834127525152",
+  appId: "1:834127525152:web:1d117b2d5b11d5fc2a1345",
+  measurementId: "G-4XLNMZDDJJ",
 };
 
 export const isFirebaseConfigured = Object.values(firebaseConfig).every(
